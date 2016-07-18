15b2ca0b98460d4df19e124801dbf11ffe0a2fec
Kite <ixkaito@gmail.com>
2016-07-18 23:43:43 +0900

    Level 7

---
+++ level-7.js
@@ -0,0 +1,23 @@
+jsWarrior.turn = function(warrior) {
+  if (!warrior._health) {
+    warrior._health = warrior.getHealth();
+  }
+
+  if (warrior.check() == 'wall') {
+    warrior.pivot();
+  } else if (warrior.check() == 'enemy') {
+    warrior.attack();
+  } else if (warrior.getHealth() < warrior._health) {
+    if (warrior.getHealth() < 10) {
+      warrior.walk('backward');
+    } else {
+      warrior.walk();
+    }
+  } else if (warrior.getHealth() < 20) {
+    warrior.rest();
+  } else {
+    warrior.walk();
+  }
+
+  warrior._health = warrior.getHealth();
+}
