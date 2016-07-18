20d411b1b55c7e672ea724547a3ca6fa22df5638
Kite <ixkaito@gmail.com>
2016-07-18 23:03:14 +0900

    Level 4

---
+++ level-4.js
@@ -0,0 +1,11 @@
+jsWarrior.turn = function(warrior) {
+  warrior._health = warrior._health ? warrior._health : warrior.getHealth();
+  if (warrior.check() == 'enemy') {
+    warrior.attack();
+  } else if (warrior.getHealth() < 20 && warrior.getHealth() < warrior._health) {
+    warrior.rest();
+  } else {
+    warrior.walk();
+  }
+  warrior._health = warrior.getHealth();
+}
